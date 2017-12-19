ae3e378855d0403484022e18c2c6ce564d4ef227
slie07 <liestevano@gmail.com>
2017-12-19 12:45:31 -0700

    Add files via upload

--- js-ajax-lab-master/starter-code/app/app.js
+++ js-ajax-lab-master/starter-code/app/app.js
@@ -9,7 +9,32 @@ $.get('https://super-crud.herokuapp.com/books')
         $('#books').append($('<li>').text(data.books[i].releaseDate));
       }
     });
-      // for (let i=0; i<data.books.length; i++){
-      //     $(‘#books’).append($(‘<li>’).text(JSON.stringify(data.books[i])));
 
-      // 
+
+var formdata = 
+$(“form#new-book”).submit(function() {
+   event.preventDefault();    
+   var newImage = $(“#book-image”).val();
+   var newTitle = $(“#book-title”).val();
+   var newAuthor = $(“#book-author”).val();
+   var newDate = $(“#release-date”).val();
+   var list = $(“ul#books”);
+       list.append(“<img src=” + newImage + “></img>“);
+       list.append(“<li>” + newTitle + “</li>“);
+       list.append(“<li>” + newAuthor + “</li>“);
+       list.append(“<li>” + newDate + “</li>“);
+  
+
+  $.ajax({
+      type: ‘POST’,
+      url: ’https://den-super-crud.herokuapp.com/books',
+      data: {
+          “image”: newImage,
+          “title”: newTitle,
+          “author”: newAuthor,
+          “releaseDate”: newDate,
+      },
+      dataType: ‘JSON’
+  }); 
+
+});
